f130bb94125adf254971d33b4db1481aba7df1bd
Student IT0213 <Student0213@gbas.local>
2025-09-30 13:56:52 +0200

    Fixing gitignore

--- Scripts/Gen-Visual-Studio-Code-Files.bat
+++ Scripts/Gen-Visual-Studio-Code-Files.bat
@@ -1,7 +1,7 @@
 @echo off
 
 pushd %~dp0\..\
-call Programs\Premake\premake5.exe --file=Nebula.lua gmake2
-call Programs\Premake\premake5.exe --file=Nebula.lua vscode
+call Programs\Premake\premake5.exe --file=Starlight.lua gmake2
+call Programs\Premake\premake5.exe --file=Starlight.lua vscode
 popd
 PAUSE
